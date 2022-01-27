1594c353b14f8883c247e3f46c8eeb38f35e0972
Burak Bay <burak.bay@microlise.com>
2022-01-27 20:21:39 +0000

    Adds the DatabaseContext

---
+++ Ensek.TechnicalTest/Ensek.TechnicalTest.Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Ensek.TechnicalTest.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ensek.TechnicalTest.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<MeterReading> MeterReadings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MeterReading>().HasOne(x => x.Account).WithMany();
+        }
+    }
+}
