e73f26ac04c0d4cfb09f27e9159e03fcd19ae5f4
Burak Bay <burak.bay@microlise.com>
2022-01-27 20:20:17 +0000

    Adds the Account Model

---
+++ Ensek.TechnicalTest/Ensek.TechnicalTest.Database/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ensek.TechnicalTest.Database.Models
+{
+    [Table(nameof(Account))]
+    public class Account
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int AccountId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
